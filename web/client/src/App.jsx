bee9be5ab4ce15979837f058badd7ea6135cdd6f
Ajitesh <Ajitesh72>
2024-05-06 23:26:51 +0530

    client added

---
+++ web/client/src/App.jsx
@@ -0,0 +1,47 @@
+import logo from "../src/assets/logo.png";
+import "./App.css";
+
+function App() {
+  const openGithub = () => {
+    window.open("https://github.com/Ajitesh72/BrowseTrack", "_blank");
+  };
+  return (
+    <div className="app-container">
+      <div className="navbar">
+        <div className="brand-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="image-hai w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
+          />
+        </svg>
+          <span className="brand-name">BrowseTrack</span>
+        </div>
+        <div className="github" onClick={openGithub}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="image-hai-1 w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+</svg>
+
+          <span>Github</span>
+        </div>
+      </div>
+      <div className="dashboard-main">
+        <iframe
+          src="http://localhost:5601/goto/cebc0832200b2c596872dedf27c0df9a"
+          title="Kibana Dashboard"
+          className="dashboard-frame"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
